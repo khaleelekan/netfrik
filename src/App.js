caa64041c1062cb42c9fea5af29283b87eec952d
unknown <kalilulekan@gmail.com>
2023-01-30 11:44:36 +0100

    assets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRoute, Routes, route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRoute>
+    <Routes>
+     {/* <Route exact path> */}
+    </Routes>
+    </BrowserRoute>
+  )
 }
 
-export default App;
+export default App
