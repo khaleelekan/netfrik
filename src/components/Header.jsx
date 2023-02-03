9684c2a75305eebda9c7f5e0bec8a82cad314fdf
unknown <kalilulekan@gmail.com>
2023-02-03 23:04:06 +0100

    stylings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,5 +17,21 @@ export default function Header(props) {
   )
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+padding: 0 4rem;
+.logo {
+  img {
+    height: 5rem;
+  }
+}
+button {
+  padding: 0.5rem 1rem;
+  background-color: #e50914;
+  border: none;
+  cursor: pointer;
+  color: white;
+  border-radius: 0.2rem;
+  font-weight: bolder;
+  font-size: 1.05rem;
+}`;
 
