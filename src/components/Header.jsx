6c35c7633c665a928c79fd1cc682641f1ba92c21
unknown <kalilulekan@gmail.com>
2023-02-03 23:01:56 +0100

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components'
 import logo from '../assets/logo.png';
 
-export default function Header() {
+export default function Header(props) {
+  const navigate = useNavigate();
   return (
     <Container>
       <div className='logo'>
         <img src={logo} alt='logo'/>
       </div>
+      <button onClick={()=> navigate(props.login ? '/login' : '/signup')}>
+        {props.login? 'Log In' : 'Sign In'}
+      </button>
     </Container>
   )
 }
