bdedb71b7f381aa3d5a18269b3cc5280976c3c87
unknown <kalilulekan@gmail.com>
2023-02-03 22:48:22 +0100

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
+import logo from '../assets/logo.png';
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <Container>
+      <div className='logo'>
+        <img src={logo} alt='logo'/>
+      </div>
+    </Container>
   )
 }
+
+const Container = styled.div``;
+
