0d0502d164248190c927d3531544be0dcfa5f233
unknown <kalilulekan@gmail.com>
2023-02-07 16:55:13 +0100

    navbar $ netflix

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useState } from 'react'
+import Navbar from '../components/Navbar'
 
 function Netflix() {
+  const [isScrolled, setIsScrolled] = useState (false)
+  
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true)
+    return () => (window.onscroll = null)
+  }
   return (
-    <div>Netflix</div>
+    <div>
+      <Navbar isScrolled={isScrolled}/>
+    </div>
   )
 }
 
