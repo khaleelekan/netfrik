efc99297eae7881330232f51661d8830544fb703
unknown <kalilulekan@gmail.com>
2023-02-07 17:12:14 +0100

    style bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Navbar() {
+    const links = [
+        { name: "Home", link: "/" },
+        { name: "TV Shows", link: "/tv" },
+        { name: "Movies", link: "/movies" },
+        { name: "My List", link: "/mylist" },
+      ];
   return (
-    <div>Navbar</div>
+  <Container>
+
+  </Container>
   )
+
+ 
 }
+const Container = styled.div;
 
 export default Navbar
