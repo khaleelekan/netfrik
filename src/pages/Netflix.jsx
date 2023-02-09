c274b0d9631268525d915d00b62a370b253a861b
unknown <kalilulekan@gmail.com>
2023-02-09 11:52:58 +0100

    netflix page

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { useState } from 'react'
 import Navbar from '../components/Navbar'
+import backgroundImage from '../assets/home.jpg'
+import MovieLogo from '../assets/homeTitle.webp'
+import {FaPlay} from 'react-icons/fa'
+import { useNavigate } from 'react-router-dom'
+import {AiOutlineInfoCircle} from 'react-icons/ai'
+
 
 function Netflix() {
   const [isScrolled, setIsScrolled] = useState (false)
@@ -9,10 +15,34 @@ function Netflix() {
     setIsScrolled(window.pageYOffset === 0 ? false : true)
     return () => (window.onscroll = null)
   }
+
+  const navigate = useNavigate();
   return (
     <div>
       <Navbar isScrolled={isScrolled}/>
-    </div>
+      <div className="hero">
+        <img
+          src={backgroundImage}
+          alt="background"
+          className="background-image"
+        />
+        <div className="container">
+          <div className="logo">
+            <img src={MovieLogo} alt="Movie Logo" />
+          </div>
+          <div className="buttons flex">
+            <button
+              onClick={() => navigate("/player")}
+              className="flex j-center a-center"
+            >
+              <FaPlay />
+              Play
+            </button>
+   </div>
+   </div>
+   </div>
+   </div>
+   
   )
 }
 
