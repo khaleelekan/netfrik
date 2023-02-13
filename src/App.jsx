820b117f83fb157ced7cfe7fb120841b0d927396
unknown <kalilulekan@gmail.com>
2023-02-13 13:41:03 +0100

    player

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Netflix from "./pages/Netflix";
+import Player from "./pages/Player";
 
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<Signup />} />
         <Route exact path="/" element={<Netflix />} />
+        <Route exact path="/player" element={<Player />} />
       </Routes>
     </BrowserRouter>
   );
